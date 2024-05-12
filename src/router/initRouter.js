3ab5cd9be1e6998dbcd3c2c391b5344615a601f5
chang <kennethchangla@gmail.com>
2024-05-12 21:57:11 +0800

    refactor/ add page folder

--- src/router/initRouter.js
+++ src/router/initRouter.js
@@ -1,9 +1,9 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
-import StoryRefactor from '../components/StoryRefactor.vue';
-import HelloWorld from '../components/HelloWorld.vue';
-import Chat from '../components/Chat.vue';
-import Attractions from "../components/Attractions.vue"
-import Attraction from "../components/Attraction.vue"
+import StoryRefactor from '../page/StoryRefactor.vue';
+import HelloWorld from '../page/HelloWorld.vue';
+import Chat from '../page/Chat.vue';
+import Attractions from "../page/Attractions.vue"
+import Attraction from "../page/Attraction.vue"
 
 const routes = [
     { path: '/', component: HelloWorld },
