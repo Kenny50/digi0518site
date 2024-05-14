b1a10a7d1debcb2d45d92dccd90467eecafdf595
chang <kennethchangla@gmail.com>
2024-05-14 15:04:43 +0800

    feat/ add itinerary page and navigate to stroy

---
+++ src/page/Itineraries.vue
@@ -0,0 +1,56 @@
+<template>
+    <q-card v-for="(itinerary, index) in itineraries" :key="index" class="my-card" flat bordered
+        @click="navigateToStory(itinerary.id)">
+        <q-card-section horizontal class="card-section" @mouseover="hoverEffect(index, true)"
+            @mouseleave="hoverEffect(index, false)" :class="{ 'hovered': isHovered[index] }">
+            <q-img class="col-5 rounded-borders" :src="itinerary.cover" />
+            <q-card-section>
+                <p>{{ itinerary.description }}</p>
+            </q-card-section>
+        </q-card-section>
+    </q-card>
+</template>
+
+<script>
+import AttractionApi from "../api/AttractionApi";
+
+export default {
+    data() {
+        return {
+            itineraries: [],
+            isHovered: []
+        };
+    },
+    methods: {
+        async getAllAttractions() {
+            try {
+                const response = await AttractionApi.getAllAttractions();
+                this.itineraries = response.itineraries;
+                console.log(this.itineraries)
+                this.isHovered = new Array(this.itineraries.length).fill(false);
+            } catch (error) {
+                console.error("Error fetching itineraries:", error);
+            }
+        },
+        hoverEffect(index, hover) {
+            this.$set(this.isHovered, index, hover);
+        },
+        navigateToStory(itineraryId) {
+            this.$router.push(`/story/${itineraryId}`);
+        }
+    },
+    created() {
+        this.getAllAttractions();
+    }
+};
+</script>
+
+<style scoped>
+.card-section {
+    transition: all 0.3s ease;
+}
+
+.hovered {
+    transform: scale(1.05);
+}
+</style>
--- src/router/initRouter.js
+++ src/router/initRouter.js
@@ -3,13 +3,14 @@ import StoryRefactor from '../page/StoryRefactor.vue';
 import Intro from '../page/Intro.vue';
 import Form from '../page/Form.vue';
 import Attractions from "../page/Attractions.vue"
+import Itineraries from "../page/Itineraries.vue"
 
 const routes = [
     { path: '/', component: Intro },
     { path: '/story', component: StoryRefactor },
     { path: '/story/:itineraryId', component: StoryRefactor },
     { path: '/attractions', component: Attractions },
-    { path: '/itineraries', component: Attractions },
+    { path: '/itineraries', component: Itineraries },
     { path: '/form', component: Form },
     { path: '/dashboard', component: Form }
 ]
