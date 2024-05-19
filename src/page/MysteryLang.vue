d0f0743d7313ffb92ca5424d4de85bf12af62655
chang <kennethchangla@gmail.com>
2024-05-19 10:15:10 +0800

    lang dialog label and persistent

--- src/page/MysteryLang.vue
+++ src/page/MysteryLang.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <q-dialog v-model="showDialog">
+        <q-dialog v-model="showDialog" persistent>
             <q-card>
                 <q-card-section>
                     <div class="text-h6">Please choose language/ 請選擇語言</div>
                 </q-card-section>
                 <q-card-actions align="right">
                     <q-btn label="中文" size="xl" @click="navigateToMystery" color="accent" />
-                    <q-btn label="英文" size="xl" @click="navigateToMysteryDynamic" color="accent" />
+                    <q-btn label="English" size="xl" @click="navigateToMysteryDynamic" color="accent" />
                 </q-card-actions>
             </q-card>
         </q-dialog>
@@ -24,11 +24,11 @@ export default {
     },
     methods: {
         navigateToMystery() {
-            this.showDialog = false;
+            // this.showDialog = false;
             this.$router.push('/mystery');
         },
         navigateToMysteryDynamic(param) {
-            this.showDialog = false;
+            // this.showDialog = false;
             this.$router.push(`/mystery/en`);
         },
     },
